17a3f8fb7f3719b23f519d6a19516dc7c7224afb
Jean-LouisH <LouisLou512.e@gmail.com>
2020-01-14 10:38:04 -0500

    Added Artificial Neuron module

---
+++ ArtificialNeuron.py
@@ -0,0 +1,20 @@
+class ArtificialNeuron:
+    
+    def __init__(self):
+        self.activationLevel = 0
+        self.inputs = []
+        self.output = 0
+
+    def receiveInputs(self, previousLayerOutputs, inputWeights):
+        self.inputs.clear()
+        #
+        pass
+        
+    def computeActivationLevel():
+        pass
+    
+    def heavySideStepFunction(self, activationLevel):
+        if self.activationLevel >= 0:
+            self.output = 1
+        else:
+            self.output = 0
